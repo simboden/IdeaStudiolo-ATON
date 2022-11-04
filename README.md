f9892be5700f0d89937c6f4c1df324400b015396
Bruno Fanini <phoenixbf@gmail.com>
2022-11-04 10:13:28 +0100

    initial app refactoring for plug&play arch

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # IdeaStudiolo-ATON
 Idea Studiolo packaged as an ATON custom webapp
+
+# Steps
+1) Follow [ATON setup instructions](https://github.com/phoenixbf/aton) to have the framework up & running.
+
+2) git clone this folder in your ATON wapps/ folder (e.g. wapps/studiolo/) and just open http://localhost:8080/a/studiolo/ on your browser.
---
+++ src/main.js
@@ -0,0 +1,57 @@
+/*
+	Studiolo App
+
+==============================================================*/
+
+let APP = ATON.App.realize();
+window.APP = APP;
+
+// This is our assets root folder
+APP.assetsPath = APP.basePath + "content/";
+
+// Tell ATON to look for 3D content here
+ATON.setPathCollection(APP.assetsPath);
+
+// Interaction Modes
+APP.MODE_FP = 0;    // First Person
+APP.MODE_TP = 1;    // Third Person
+APP.MODE_CU = 2;    // Close-up
+
+// APP.setup() is required for web-app initialization
+// You can place here UI setup (HTML), events handling, etc.
+APP.setup = ()=>{
+    console.log( APP.assetsPath );
+
+    ATON.FE.realize(); // Realize the base front-end
+	ATON.FE.addBasicLoaderEvents(); // Add basic events handling
+
+    APP.setupUI();
+
+    APP.currMode = APP.MODE_FP;
+};
+
+// Setup for HTML UI
+APP.setupUI = ()=>{
+
+};
+
+APP.changeInteractionMode = (mode)=>{
+    if (mode === APP.currMode) return;
+
+    // TODO: ...
+
+    APP.currMode = mode;
+};
+
+/* APP.update() if you plan to use an update routine (executed continuously)
+APP.update = ()=>{
+
+};
+*/
+
+
+
+// Run the App
+window.addEventListener('load',()=>{
+    APP.run();
+});
