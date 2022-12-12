12641915bee0f223deeb7e9a1f84043e98bd60c7
Bruno Fanini <phoenixbf@gmail.com>
2022-12-12 16:00:11 +0100

    sample switch FP/CU modes on demo scene; sample trigger logic

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ let APP = ATON.App.realize();
 window.APP = APP;
 
 // This is our assets root folder
-//APP.assetsPath = APP.basePath + "content/";  //SIL:  APP.basePath==undefined
-APP.assetsPath = "/a/studiolo/content/";
+APP.assetsPath = APP.basePath + "content/";  //SIL:  APP.basePath==undefined (BF: use latest ATON version)
+//APP.assetsPath = "/a/studiolo/content/";
 
 // Tell ATON to look for 3D content here
 ATON.setPathCollection(APP.assetsPath);
@@ -18,6 +18,10 @@ APP.MODE_FP = 0;    // First Person
 APP.MODE_TP = 1;    // Third Person
 APP.MODE_CU = 2;    // Close-up
 
+// POVs
+APP.POV_HOME = new ATON.POV().setPosition(-1.8,1.6,-1.0).setTarget(-1.8, 0.8, -6.8).setFOV(75.0);
+
+
 // APP.setup() is required for web-app initialization
 // You can place here UI setup (HTML), events handling, etc.
 APP.setup = ()=>{
@@ -25,15 +29,42 @@ APP.setup = ()=>{
 
     ATON.FE.realize(); // Realize the base front-end
 	ATON.FE.addBasicLoaderEvents(); // Add basic events handling
+    APP.setupEvents();
     
     ATON.createSceneNode("demo").load("virtual_studiolo_boxes.glb").attachToRoot(); // SIL
+
+    APP.setupTriggers();
     
     APP.setupUI();
 
     APP.currMode = APP.MODE_FP;
+    ATON.Nav.setFirstPersonControl();
+    ATON.Nav.setAndRequestHomePOV(APP.POV_HOME);
 };
 
-// Setup for HTML UI
+APP.setupTriggers = ()=>{
+    ATON.createSemanticNode("sample1").load("triggers/sample-trigger1.glb").attachToRoot();
+};
+
+// Here we handle events
+APP.setupEvents = ()=>{
+
+    ATON.on("AllNodeRequestsCompleted", ()=>{ 
+        // All resources loaded
+    });
+
+    // A sample switch to CloseUp when double-tapping on valid trigger
+    ATON.on("DoubleTap", (e)=>{
+        let S = ATON.getHoveredSemanticNode();
+
+        if (S){
+            APP.changeInteractionMode(APP.MODE_CU);
+            ATON.Nav.requestPOVbyNode(S);
+        }
+    });
+};
+
+// Setup HTML UI
 APP.setupUI = ()=>{
 
 };
@@ -41,7 +72,15 @@ APP.setupUI = ()=>{
 APP.changeInteractionMode = (mode)=>{
     if (mode === APP.currMode) return;
 
-    // TODO: ...
+    if (mode === APP.MODE_FP){
+        ATON.Nav.setFirstPersonControl();
+        ATON.Nav.requestHome(APP.POV_HOME);
+    }
+
+    if (mode === APP.MODE_CU){
+        ATON.Nav.setOrbitControl();
+        ATON.Nav._cOrbit.enablePan = false;
+    }
 
     APP.currMode = mode;
 };
